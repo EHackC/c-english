5afd7f8e351354de54b603e68ea887bfd1d68f73
EHackC <lolisfun0987@gmail.com>
2024-04-14 19:09:58 +0900

    :>

---
+++ c_english.h
@@ -0,0 +1,43 @@
+#ifndef _CENGLISH_H
+#define _CENGLISH_H
+
+// ----------------------------------------------------------------
+// -- data type
+// ----------------------------------------------------------------
+
+#define character char
+#define short_integer short
+#define integer int
+#define floating_point float
+#define double_precision_floating_point double
+#define long_integer long
+
+
+// ----------------------------------------------------------------
+// -- function
+// ----------------------------------------------------------------
+
+#define An_(x) x
+#define _return_type_(x) x
+#define _function_is_defined_as_follows (){
+#define End_the_block }
+
+
+// ----------------------------------------------------------------
+// -- keyword
+// ----------------------------------------------------------------
+
+// return
+#define Returns_the_value_(x) return x
+
+
+// ----------------------------------------------------------------
+// -- stdio.h
+// ----------------------------------------------------------------
+
+// puts
+#define Prints_the_character_(x) puts(x
+#define _to_the_screen );
+
+
+#endif
